3ebaa7663ab8be1d7a58b7ecdf56128154888798
langyo <langyo.china@gmail.com>
2019-08-14 23:26:05 +0800

    将 store 独立出来，方便 socket 进行 dispatch

--- client/socketServer.js
+++ client/socketServer.js
@@ -7,7 +7,7 @@ module.exports = (server, client) => new Promise(resolve => {
   
   client.onOpen(() => {
     // Electron 渲染进程 -> Electron 主进程 -> 服务器
-    events.forEach(n => server.register(n, obj => client.send({ ...obj, type: 'execute' })), n);
+    events.forEach(n => server.register(n, obj => client.send({ ...obj, type: 'execute' })));
     // 服务器 -> Electron 主进程 -> Electron 渲染进程
     events.forEach(n => client.receive(n, obj => server.clients.forEach(client => client.send({ ...obj, type: 'data'}))));
   })
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import Main from './containers/views/main';
-import reducer from './reducer';
 import socket from './socketDriver';
-
-const store = createStore(reducer, applyMiddleware(logger, thunk));
+import store from './store';
 
 render(
   <Provider store={store}>
